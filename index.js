eacb070d4084224b828a504d17e5c59d420d4f89
raynyator <rayn.yator@student.moringaschool.com>
2023-09-26 15:05:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+function writeCards() {
+    const randomNames = ["Guadalupe"
+        , "Ollie",
+        "Aki"
+    ];
+
+    const thankYouMessages = [];
+
+    for (let i = 0; i < randomNames.length; i++) {
+        const name = randomNames[i];
+        const message = `Thank you, ${name}, for the wonderful surprise gift!`;
+        thankYouMessages.push(message);
+    }
+
+    return thankYouMessages;
+}
+
+const messages = writeCards();
+    
+console.log(messages);
+
+function countDown(thenumber){
+    while (thenumber>=0){
+        console.log(thenumber);
+       thenumber--; 
+    }
+   
+}
